21399013a409facd1fb68ef819940a3138fc4189
Josue Rojas <josuemonro@gmail.com>
2023-10-26 22:44:47 -0600

    Configuración de los archivos de tailwind y creación de la pantalla de incicio de sesión

--- front_DestinoMX/src/main.js
+++ front_DestinoMX/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/css/tailwind.css";
+import "./assets/css/styles.css";
+// import "./plugins/vuesax";
 
 createApp(App).use(store).use(router).mount("#app");
--- front_DestinoMX/src/router/index.js
+++ front_DestinoMX/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import LoginForm from "@/modules/auth/views/LoginForm.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "/login",
+    name: "login",
+    component: LoginForm,
   },
   {
     path: "/about",
