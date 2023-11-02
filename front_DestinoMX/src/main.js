38eecea00c554ea629d14593d8a44c8034e2d669
Josue Rojas <josuemonro@gmail.com>
2023-11-02 10:38:03 -0600

    Definicón de los componentes de vee-validate

--- front_DestinoMX/src/main.js
+++ front_DestinoMX/src/main.js
@@ -5,13 +5,13 @@ import store from "./store";
 import "./assets/css/tailwind.css";
 import "./assets/css/styles.css";
 import "vuesax/dist/vuesax.css"; // Estilos de Vuesax
-// import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 
 const app = createApp(App);
 
 app
   .use(store)
   .use(router)
-  // .component("ValidationObserver", ValidationObserver)
-  // .component("ValidationProvider", ValidationProvider)
+  .component("ValidationObserver", ValidationObserver)
+  .component("ValidationProvider", ValidationProvider)
   .mount("#app");
