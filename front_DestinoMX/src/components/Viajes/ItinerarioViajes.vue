68b3a37dbc1db9f794f34b99dcd71843f282dd7c
Josue Rojas <josuemonro@gmail.com>
2023-11-02 14:47:29 -0600

    Se agrega el script al componente de Itinerario

---
+++ front_DestinoMX/src/components/Viajes/ItinerarioViajes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Itinerario</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItinerarioViajes",
+  data() {},
+  methods: {},
+};
+</script>
+
+<style></style>
