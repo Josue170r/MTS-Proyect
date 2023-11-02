4c74619c69bc1e7c34821dd9d7674e67eb628b0e
Ramses Martinez <ramses.mtz96@gmail.com>
2023-11-02 17:38:41 -0600

    refactor front with vee-validate

--- front_DestinoMX/src/components/Viajes/GoogleMaps.vue
+++ front_DestinoMX/src/components/Viajes/GoogleMaps.vue
@@ -12,34 +12,34 @@
 </template>
 
 <script>
-import { GoogleMap, Marker } from "vue3-google-map";
+import { GoogleMap, Marker } from 'vue3-google-map'
 
 export default {
-  name: "GoogleMaps",
+  name: 'GoogleMaps',
   components: {
     GoogleMap,
     Marker,
   },
   data() {
     return {
-      apiKey: "AIzaSyA7zLTbiIG9CpbTiNfZMQZZUoPMo8kbh70",
-      initialPosition: "",
-      localitation: "",
-    };
+      apiKey: 'AIzaSyA7zLTbiIG9CpbTiNfZMQZZUoPMo8kbh70',
+      initialPosition: '',
+      localitation: '',
+    }
   },
   methods: {},
   created() {
     this.$getLocation()
       .then((coordinates) => {
-        this.initialPosition = { lat: coordinates.lat, lng: coordinates.lng };
-        console.log(this.initialPosition);
-        console.log(coordinates);
+        this.initialPosition = { lat: coordinates.lat, lng: coordinates.lng }
+        console.log(this.initialPosition)
+        console.log(coordinates)
       })
       .catch((error) => {
-        console.log(`El error es este: ${error}`);
-      });
+        console.log(`El error es este: ${error}`)
+      })
   },
-};
+}
 </script>
 
 <style></style>
