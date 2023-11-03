a2d2822ac66d6bc6f81ed06725a889053485a2c8
Josue Rojas <josuemonro@gmail.com>
2023-11-03 01:53:54 -0600

    Correcciones en la pantalla de mapa interactivo

--- front_DestinoMX/src/components/Viajes/GoogleMaps.vue
+++ front_DestinoMX/src/components/Viajes/GoogleMaps.vue
@@ -3,7 +3,7 @@
     <div
       class="flex rounded-2xl items-center justify-center bg-orange-300 w-full"
     >
-      <BackButton class="mx-2" />
+      <BackButton class="mx-2 mt-2" />
       <h1 class="text-white py-8 text-center text-xl">
         ¡Explora lugares cerca de ti!
       </h1>
@@ -12,7 +12,7 @@
       @click="SelectedPlace"
       :api-key="apiKey"
       mapTypeId="terrain"
-      :style="{ width: '100%', height: isEmpyCurrenName ? '90%' : '80%' }"
+      :style="{ width: '100%', height: isEmpyCurrenName ? '90%' : '73%' }"
       :center="relativePosition"
       :zoom="17"
     >
@@ -20,14 +20,35 @@
     </GoogleMap>
     <div
       v-if="CurrentNamePlace"
-      class="flex rounded-full items-center justify-center bg-white w-full flex-col"
+      class="flex rounded-2xl items-center justify-center bg-white w-full flex-col"
     >
-      <h1 class="text-gray-800 py-3 text-center text-xl">
-        {{ CurrentNamePlace }}
-      </h1>
+      <div class="flex flex-col items-center w-full">
+        <div class="flex items-center">
+          <LocalitationIcon />
+          <h1 class="ml-3 text-gray-800 py-3 text-center text-xl">
+            {{ CurrentNamePlace }}
+          </h1>
+        </div>
+        <div class="flex justify-around">
+          <h2 class="mx-10 mb-3">
+            Distancia: <span class="font-bold">60m</span>
+          </h2>
+          <h2 class="mx-10 mb-3">
+            Tiempo: <span class="font-bold">60min</span>
+          </h2>
+        </div>
+      </div>
       <div class="flex justify-around w-full">
-        <button>Ver descripción</button>
-        <button>Visualizar ruta</button>
+        <button
+          class="font-quicksand w-40 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+        >
+          Ver descripción
+        </button>
+        <button
+          class="font-quicksand w-40 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+        >
+          Visualizar ruta
+        </button>
       </div>
     </div>
   </div>
@@ -37,6 +58,7 @@
 import { GoogleMap, Marker } from "vue3-google-map"
 import BackButton from "@/components/buttons/BackButton"
 import { getNameApi } from "@/components/Viajes/helpers/ApiPlaceName"
+import LocalitationIcon from "@/components/icons/LocalitationIcon"
 
 export default {
   name: "GoogleMaps",
@@ -44,6 +66,7 @@ export default {
     GoogleMap,
     Marker,
     BackButton,
+    LocalitationIcon,
   },
   data() {
     return {
@@ -57,6 +80,7 @@ export default {
   },
   methods: {
     SelectedPlace(event) {
+      console.log(event.placeId)
       this.getNamePlace(event.placeId)
     },
     async getNamePlace(placeId) {
