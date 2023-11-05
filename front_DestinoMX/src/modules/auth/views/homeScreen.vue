602b719f5f3db886e5441cf249e2e5ca2294ed97
JerryOrt <97775563+IPJerry@users.noreply.github.com>
2023-11-04 21:41:53 -0600

    Se agrago el icono de busqueda

--- front_DestinoMX/src/modules/auth/views/homeScreen.vue
+++ front_DestinoMX/src/modules/auth/views/homeScreen.vue
@@ -7,9 +7,11 @@
         alt="imagen003"
         class="w-full h-full object-cover"
       />
-      <div class="w-full h-full object-cover">
-        <input type="text" class="w-full rounded p-2" placeholder="Buscar..." />
-      </div>
+      <input
+        type="text"
+        class="mt-16 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2 bg-white border border-gray-300 rounded-md"
+        placeholder="Buscar..."
+      />
     </div>
     <div>
       <a>Primera parte</a>
