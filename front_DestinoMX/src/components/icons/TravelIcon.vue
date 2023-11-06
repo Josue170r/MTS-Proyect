10726addfc8e95f162f038da7ccf66a1ee65e1af
JerryOrt <97775563+IPJerry@users.noreply.github.com>
2023-11-05 22:43:07 -0600

    Se agregan cambios del Burger Button

--- front_DestinoMX/src/components/icons/TravelIcon.vue
+++ front_DestinoMX/src/components/icons/TravelIcon.vue
@@ -5,24 +5,23 @@
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="w-6 h-6 text-gray-400 color-stroke"
+    class="w-6 h-6 color-stroke"
   >
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
-      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+      d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
     />
   </svg>
 </template>
-
 <script>
 export default {
-  name: "UserIcon",
+  name: "TravelIcon",
 }
 </script>
 
 <style scoped>
 .color-stroke path {
-  stroke: purple;
+  stroke: blue;
 }
 </style>
