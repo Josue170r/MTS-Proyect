5946aad72cde22aed71e700a85a8e95524f2d9ca
Josue Rojas <josuemonro@gmail.com>
2023-11-06 19:43:54 -0600

    Se agrega pantalla NotFound 404

---
+++ front_DestinoMX/src/screens/NotFoundPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img
+    src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+    alt=""
+  />
+</template>
+
+<script>
+export default {
+  name: "NotFoundScreen",
+}
+</script>
+
+<style></style>
