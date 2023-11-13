c003c0432a32d65c900885ba8422c5e2d6a23b83
Josue Rojas <josuemonro@gmail.com>
2023-11-13 14:48:02 -0600

    Modificación de las dependencias en los plugins

--- front_DestinoMX/src/main.js
+++ front_DestinoMX/src/main.js
@@ -7,15 +7,8 @@ import "./assets/css/styles.css"
 import "./assets/css/vuetify.css"
 import Vue3Geolocation from "vue3-geolocation"
 import { vuetify } from "./plugins/vuetify"
-import { toast } from "./plugins/toastifyNotification"
 // para el divider
 import "vuetify/dist/vuetify.min.css"
 
 const app = createApp(App)
-app
-  .use(store)
-  .use(router)
-  .use(toast)
-  .use(Vue3Geolocation)
-  .use(vuetify)
-  .mount("#app")
+app.use(store).use(router).use(Vue3Geolocation).use(vuetify).mount("#app")
