212606141b61d74801aa762855977a8c997e6d21
Rogan52277 <121593831+Rogan52277@users.noreply.github.com>
2023-11-16 21:28:42 -0600

    creacion de preferencias

---
+++ back_DestinoMX/src/routes/preferencias.js
@@ -0,0 +1,10 @@
+import { mySqlConnection } from "../DB/DB_connection.js";
+import { Router } from "express";
+
+export const routerAutenticacion = Router();
+
+//Consulta - Angel
+
+//Insercion - Pedro
+
+//Modificacion - Rodrigo
