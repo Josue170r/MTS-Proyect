a6427adcd843301d0290eb30d5ede58cb001a43d
JerryOrt <97775563+IPJerry@users.noreply.github.com>
2023-11-19 06:11:46 -0600

    Se agrega diseño responsivo a la ventana emergente

--- front_DestinoMX/src/components/Viajes/searchPlace.vue
+++ front_DestinoMX/src/components/Viajes/searchPlace.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="showModal" class="fixed inset-0 overflow-y-auto z-50">
     <div
-      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      class="flex items-center items-end min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 w-full justify-center"
     >
       <div class="fixed inset-0 transition-opacity" @click="closeModal">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
