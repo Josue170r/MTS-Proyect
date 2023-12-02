713f8dc3219883c90b2958ab544d495ac03bdb19
Mich090801 <mich_girl09@hotmail.com>
2023-12-02 02:47:15 -0600

    Cambios

--- front_DestinoMX/src/components/Viajes/ItinerarioViajes.vue
+++ front_DestinoMX/src/components/Viajes/ItinerarioViajes.vue
@@ -19,6 +19,71 @@
         <button class="bg-orange-300 text-gray-700 p-2 rounded-r-md"></button>
         <SearchButton class="bg-black" />
         -->
+        <!--INICIO BARRA DE BUSQUEDA COPIA DEL HOME-->
+        <div
+          class="mt-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2 bg-white flex items-center shadow-md"
+          :class="[
+            places.length > 5
+              ? 'rounded-t-xl rounded-b-xl'
+              : 'rounded-t-xl rounded-b-xl',
+          ]"
+        >
+          <div class="relative">
+            <div class="flex items-center">
+              <input
+                v-model="namePlaceToFind"
+                type="text"
+                class="w-64 md:w-96 px-2 py-2 outline-none border-none flex-grow mr-2"
+                placeholder="Busca tu próximo DestinoMX ..."
+                @input="FindPlacesFromInput"
+              />
+              <button v-if="namePlaceToFind.length > 0" @click="closeSearch">
+                <CloseIcon class="text-left" />
+              </button>
+            </div>
+            <ul
+              ref="placesList"
+              class="absolute w-72 mt-3 mr-8 bg-white rounded-b shadow-md overflow-hidden rounded-md"
+              v-if="places.length > 5 && showSearch"
+            >
+              <li
+                v-for="(place, index) in places.slice(0, 5)"
+                :key="index"
+                class="w-full py-2 border-b last:border-b-0 transition duration-300 ease-in-out hover:bg-orange-100 focus:bg-orange-200"
+              >
+                <div>
+                  <button
+                    class="w-full py-2 px-4 transition-all duration-300 ease-in-out hover:bg-orange-100 hover:text-gray-800 focus:text-gray-800 rounded-md transition duration-300 ease-in-out transform-gpu"
+                    :class="{
+                      'bg-orange-100 text-gray-800 focus:text-gray-800':
+                        isSelected,
+                    }"
+                    @click="handleButtonClick"
+                  >
+                    <div class="flex items-center">
+                      <img
+                        :src="place.icon"
+                        class="w-6 h-6 brightness object-cover rounded-md mr-2"
+                      />
+                      <div class="flex flex-col">
+                        <h1
+                          class="text-sm text-lowercase text-left cursor-pointer transition-all duration-300 ease-in-out transform-gpu"
+                          @click="goToPlaceDescriptionFromSearch(place)"
+                        >
+                          {{ place.name }}
+                        </h1>
+                        <h1 class="text-sm text-gray-500">
+                          {{ place.address }}
+                        </h1>
+                      </div>
+                    </div>
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!--FIN BARRA DE BUSQUEDA COPIA DEL HOME  -->
       </div>
 
       <div class="absolute top-6 right-2 transform -translate-x-1">
