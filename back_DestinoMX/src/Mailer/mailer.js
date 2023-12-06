db12757c14cd48cb242103dabeb3a5da622ce01b
Rogan52277 <121593831+Rogan52277@users.noreply.github.com>
2023-12-05 19:06:21 -0600

    Api de envio de correos Completa

--- back_DestinoMX/src/Mailer/mailer.js
+++ back_DestinoMX/src/Mailer/mailer.js
@@ -15,15 +15,38 @@ const from = "mtsads77@gmail.com"
 
 
 mailerApi.post("/api/mailer",(req,res)=>{
-
+    
     const {to, subject, text} = req.body
+ 
+    if(!to || !subject || !text){
+        return res.status(400).json({
+            exito:false,
+            mensaje:"Faltan mas parametros"
+        })
+    }
+    
+    console.log(req.body);
 
     const mailOptions = {
         from,
         to,
         subject,
-        html:''
-    };
+        html:`
+        <html>
+        <body>
+        <table border="1" style="height: 59px; width: 96.9913%; border-collapse: collapse; background-color: #ffb833;" height="120">
+        <tbody>
+        <tr>
+        <td style="width: 100%; text-align: center;"><strong><h1>MTS, Mexico Travel Solucions</h1></strong></td>
+        </tr>
+        </tbody>
+        </table>
+        <p style="text-align: justify;"><h2>Hola, muchas gracias por utilizar MTS, para sus soluciones de viajes.</h2></p>
+        <p style="text-align: justify;">${text}</p>
+        </body>
+        </html>
+        `
+      };
     transporte.sendMail(mailOptions, (error, info) => {
         
         if (error) {
