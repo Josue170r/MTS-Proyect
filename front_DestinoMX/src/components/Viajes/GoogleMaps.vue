2a792bf56b53bbc2550fa6b08ad7b045b9346809
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2023-12-13 22:43:43 -0600

    ...

--- front_DestinoMX/src/components/Viajes/GoogleMaps.vue
+++ front_DestinoMX/src/components/Viajes/GoogleMaps.vue
@@ -149,7 +149,6 @@ export default {
       this.$router.push({
         name: "placedescription",
         query: {
-          //manda el placeID del lugar seleccionado a PlaceDescription
           placeid: this.placeID,
         },
       })
