ff4949b123dd3a8e4cf7ca0a03e0da5b7b86dbf5
Josue Rojas <josuemonro@gmail.com>
2023-12-18 18:09:05 -0600

    Pruebas enm producción

--- back_DestinoMX/src/DB/DB_connection.js
+++ back_DestinoMX/src/DB/DB_connection.js
@@ -1,14 +1,22 @@
 import mysql from "mysql";
 
-export const mySqlConnection = mysql.createConnection({
-  host: "localhost",
+// export const mySqlConnection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "mts_database",
+//   port: "3345",
+// });
+
+export const mySqlConnectionProduction = mysql.createConnection({
+  host: "roundhouse.proxy.rlwy.net",
   user: "root",
-  password: "123456",
-  database: "mts_database",
-  port: "3345",
+  password: "1gdBgfeDghGD53a6C2dDf-Ed31D2bE5H",
+  database: "railway",
+  port: "51052",
 });
 
-mySqlConnection.connect((err) => {
+mySqlConnectionProduction.connect((err) => {
   if (err) {
     console.log(err);
     return;
